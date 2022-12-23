01435b210263bde7ee29524ba3fdcdb49f854dce
Suat-17 <yargicisuat@gmail.com>
2022-12-23 15:57:11 +0300

    feat: updated

--- components/ui/Search.jsx
+++ components/ui/Search.jsx
@@ -2,36 +2,36 @@ import Image from "next/image";
 import React from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 import Title from "../ui/Title";
-import { GiCancel } from "react-icons/gi";
+import { GiCancel, GiHamburgerMenu } from "react-icons/gi";
 
-const Search = ({ setIsSearchModal }) => {
+const Search = ({ setIsSearchModal}) => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen z-50 after:content-[''] after:w-screen after:h-screen after:bg-white after:absolute after:top-0 after:left-0 after:opacity-60 grid place-content-center">
+    <div className="fixed top-0 left-0 z-50 grid h-screen w-screen place-content-center after:absolute after:top-0 after:left-0 after:h-screen after:w-screen after:bg-white after:opacity-60 after:content-['']">
       <OutsideClickHandler onOutsideClick={() => setIsSearchModal(false)}>
-        <div className="w-full h-full grid place-content-center relative">
-          <div className="relative z-50 w-[600px] h-[600px] bg-white border-2 p-5">
+        <div className="relative grid h-full w-full place-content-center">
+          <div className="relative z-50 w-[370px] rounded-3xl border-2 bg-white p-10 md:w-[600px]">
             <Title addClass="text-[40px] text-center">Search</Title>
             <input
               type="text"
               placeholder="Search..."
-              className="border w-full my-10"
+              className="my-10 w-full border"
             />
             <ul>
-              <li className="flex items-center justify-between p-1 hover:bg-primary transition-all">
+              <li className="flex items-center justify-between p-1 transition-all hover:bg-primary">
                 <div className="relative flex">
                   <Image src="/images/f1.png" alt="" width={48} height={48} />
                 </div>
                 <span className="font-bold">Good Pizza</span>
                 <span className="font-bold">$10</span>
               </li>
-              <li className="flex items-center justify-between p-1 hover:bg-primary transition-all">
+              <li className="flex items-center justify-between p-1 transition-all hover:bg-primary">
                 <div className="relative flex">
                   <Image src="/images/f1.png" alt="" width={48} height={48} />
                 </div>
                 <span className="font-bold">Good Pizza</span>
                 <span className="font-bold">$10</span>
               </li>
-              <li className="flex items-center justify-between p-1 hover:bg-primary transition-all">
+              <li className="flex items-center justify-between p-1 transition-all hover:bg-primary">
                 <div className="relative flex">
                   <Image src="/images/f1.png" alt="" width={48} height={48} />
                 </div>
@@ -40,11 +40,20 @@ const Search = ({ setIsSearchModal }) => {
               </li>
             </ul>
             <button
-              className="absolute  top-4 right-4"
-              onClick={() => setIsSearchModal(false)}
+              className="inline-block sm:hidden"
+              onClick={() => setIsMenuModal(true)}
             >
-              <GiCancel size={25} className=" transition-all" />
+              <GiHamburgerMenu className="text-xl transition-all hover:text-primary" />
             </button>
+            )
+          
+              <button
+                className="absolute top-3 right-3 p-2"
+                onClick={() => setIsSearchModal(false)}
+              >
+                <GiCancel size={24} className="text-primary transition-all" />
+              </button>
+           
           </div>
         </div>
       </OutsideClickHandler>
