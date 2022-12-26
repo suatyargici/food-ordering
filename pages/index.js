9428fa075ff6260e5f198247c32c6823c57fa622
Suat-17 <yargicisuat@gmail.com>
2022-12-26 20:17:21 +0300

    feat: about component

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Home from './home'
 import Header from '../components/layout/Header'
 import MenuWrapper from '../components/product/MenuWrapper'
+import About from '../components/About'
 const Index = () => {
   return (
     <div>
       <Header />
       <Home/>
       <MenuWrapper/>
+      <About />
     </div>
   )
 }
