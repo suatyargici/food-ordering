64b281c5472603ae29ec32d22b92a9f13686a785
Suat-17 <yargicisuat@gmail.com>
2023-01-09 17:45:41 +0300

    chore: addded link for header

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -6,45 +6,56 @@ import { useState } from "react";
 import { GiCancel, GiHamburgerMenu } from "react-icons/gi";
 import Search from "../ui/Search";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const Header = () => {
   const [isSearchModal, setIsSearchModal] = useState(false);
   const [isMenuModal, setIsMenuModal] = useState(false);
   const router = useRouter();
 
-
   return (
-    <div  className={`h-[5.5rem] z-50 relative ${
-      router.asPath === "/" ? "bg-transparent" : "bg-secondary"
-    }`}>
+    <div
+      className={`relative z-50 h-[5.5rem] ${
+        router.asPath === "/" ? "bg-transparent" : "bg-secondary"
+      }`}
+    >
       <div className="container mx-auto flex h-full items-center justify-between text-white">
         <Logo />
-            <nav
-              className={`sm:static absolute top-0 left-0 sm:w-auto sm:h-auto w-full h-screen sm:text-white text-black sm:bg-transparent bg-white sm:flex hidden  ${
-                isMenuModal === true && "!grid place-content-center"
-              }`} 
+        <nav
+          className={`absolute top-0 left-0 hidden h-screen w-full bg-white text-black sm:static sm:flex sm:h-auto sm:w-auto sm:bg-transparent sm:text-white  ${
+            isMenuModal === true && "!grid place-content-center"
+          }`}
         >
           <ul className="flex gap-x-2">
-            <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
-              <a href="">Home</a>
-            </li>
-            <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
-              <a href="">Menu</a>
-            </li>
-            <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
-              <a href="">About</a>
-            </li>
-            <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
-              <a href="">Book Table</a>
-            </li>
+            <Link href="/">
+              <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
+                <a href="">Home</a>
+              </li>
+            </Link>
+            <Link link="/menu">
+              <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
+                <a href="">Menu</a>
+              </li>
+            </Link>
+            <Link href="/about">
+              i{" "}
+              <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
+                <a href="">About</a>
+              </li>
+            </Link>
+            <Link href="/reservation">
+              <li className="cursor-pointer px-[5px] py-[10px] uppercase hover:text-primary">
+                <a href="">Book Table</a>
+              </li>
+            </Link>
             {isMenuModal && (
-            <button
-              className="absolute  top-4 right-4 z-50"
-              onClick={() => setIsMenuModal(false)}
-            >
-              <GiCancel size={25} className=" transition-all" />
-            </button>
-          )}
+              <button
+                className="absolute  top-4 right-4 z-50"
+                onClick={() => setIsMenuModal(false)}
+              >
+                <GiCancel size={25} className=" transition-all" />
+              </button>
+            )}
           </ul>
         </nav>
         <div className="flex items-center gap-x-4">
@@ -57,19 +68,18 @@ const Header = () => {
           <button onClick={() => setIsSearchModal(true)}>
             <FaSearch className="transition-all hover:text-primary" />
           </button>
-          <a href="" className="md:inline-block hidden sm">
+          <a href="" className="sm hidden md:inline-block">
             <button className="btn-primary">Order Online</button>
           </a>
           <button
-              className="sm:hidden inline-block"
-              onClick={() => setIsMenuModal(true)}
-            >
-              <GiHamburgerMenu className="text-xl transition-all hover:text-primary" />
-            </button>
+            className="inline-block sm:hidden"
+            onClick={() => setIsMenuModal(true)}
+          >
+            <GiHamburgerMenu className="text-xl transition-all hover:text-primary" />
+          </button>
         </div>
         {isSearchModal && <Search setIsSearchModal={setIsSearchModal} />}
       </div>
-     
     </div>
   );
 };
--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
-  return <div className="text-[2rem] font-dancing font-bold">Feane</div>;
+  return <Link href="/">
+  <span className="text-[2rem] font-dancing font-bold cursor-pointer">
+    Feane
+  </span>
+</Link>
 };
 
 export default Logo;
