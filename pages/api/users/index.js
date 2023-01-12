1185158bff30c2ef6a2ae4b06dbeeb653fe34d57
Suat-17 <yargicisuat@gmail.com>
2023-01-12 15:19:38 +0300

    feat: added dc connect

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,28 @@
+import User from "../../../models/User";
+import dbConnect from "../../../util/dbConnect";
+
 const handler = async (req, res) => {
-  res.status(200).json({ message: "This is the users API route" });
+  await dbConnect();
+  const { method } = req;
+
+  if (method === "GET") {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (method === "POST") {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(200).json(newUser);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
 
 export default handler;
+
