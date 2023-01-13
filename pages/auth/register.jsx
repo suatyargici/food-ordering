b5ebfe45a635107eab9cbde644473316bfba8949
Suat-17 <yargicisuat@gmail.com>
2023-01-13 10:24:05 +0300

    feat: register process

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -3,29 +3,42 @@ import Link from "next/link";
 import Input from "../../components/form/Input";
 import Title from "../../components/ui/Title";
 import * as Yup from "yup";
+import axios from "axios";
 
 const Register = () => {
-    const registerSchema = Yup.object({
-        fullName: Yup.string()
-          .required("Full name is required.")
-          .min(3, "Full name must be at least 3 characters."),
-        email: Yup.string().required("Email is required.").email("Email is invalid."),
-        password: Yup.string()
-          .required("Password is required.")
-          .min(8, "Password must be at least 8 characters.")
-          .matches(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-            "Password must contain at least one uppercase, one lowercase, one number and one special character."
-          ),
-        confirmPassword: Yup.string()
-          .required("Confirm password is required.")
-          .oneOf([Yup.ref("password"), null], "Passwords must match."),
-      });
+ 
+  const registerSchema = Yup.object({
+    fullName: Yup.string()
+      .required("Full name is required.")
+      .min(3, "Full name must be at least 3 characters."),
+    email: Yup.string()
+      .required("Email is required.")
+      .email("Email is invalid."),
+    password: Yup.string()
+      .required("Password is required.")
+      .min(8, "Password must be at least 8 characters.")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must contain at least one uppercase, one lowercase, one number and one special character."
+      ),
+    confirmPassword: Yup.string()
+      .required("Confirm password is required.")
+      .oneOf([Yup.ref("password"), null], "Passwords must match."),
+  });
 
   const onSubmit = async (values, actions) => {
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    actions.resetForm();
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/register`,
+        values
+      );
+    } catch (err) {
+      console.log(err);
+    
+     actions.resetForm(); 
   };
+}
+
   const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
     useFormik({
       initialValues: {
@@ -38,7 +51,6 @@ const Register = () => {
       validationSchema: registerSchema,
     });
 
-    
   const inputs = [
     {
       id: 1,
@@ -77,15 +89,17 @@ const Register = () => {
       touched: touched.confirmPassword,
     },
   ];
+  
+  
 
   return (
     <div className="container mx-auto">
       <form
-        className="flex flex-col items-center my-20 md:w-1/2 w-full mx-auto"
+        className="my-20 mx-auto flex w-full flex-col items-center md:w-1/2"
         onSubmit={handleSubmit}
       >
         <Title addClass="text-[40px] mb-6">Register</Title>
-        <div className="flex flex-col gap-y-3 w-full">
+        <div className="flex w-full flex-col gap-y-3">
           {inputs.map((input) => (
             <Input
               key={input.id}
@@ -95,10 +109,10 @@ const Register = () => {
             />
           ))}
         </div>
-        <div className="flex flex-col w-full gap-y-3 mt-6">
-          <button className="btn-primary">REGISTER</button>
+        <div className="mt-6 flex w-full flex-col gap-y-3">
+          <button className="btn-primary" type="submit">REGISTER</button>
           <Link href="/auth/login">
-            <span className="text-sm underline cursor-pointer text-secondary">
+            <span className="cursor-pointer text-sm text-secondary underline">
               Do you have a account?
             </span>
           </Link>
