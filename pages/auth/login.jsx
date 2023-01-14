fea7df8dc7325d91863f2c749229c97c09bfe6bc
Suat-17 <yargicisuat@gmail.com>
2023-01-14 22:25:50 +0300

    chore: update schame in logon and register

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
       .required("Password is required.")
       .min(8, "Password must be at least 8 characters.")
       .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/,
         "Password must contain at least one uppercase, one lowercase, one number and one special character."
       ),
   });
--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
       .required("Password is required.")
       .min(8, "Password must be at least 8 characters.")
       .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/,
         "Password must contain at least one uppercase, one lowercase, one number and one special character."
       ),
     confirmPassword: Yup.string()
