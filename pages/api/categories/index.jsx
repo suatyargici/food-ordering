46f36fc7da7434bcddd72021079b48c0f8eadeab
Suat-17 <yargicisuat@gmail.com>
2023-01-18 10:13:24 +0300

    chore: category get

---
+++ pages/api/categories/index.jsx
@@ -0,0 +1,27 @@
+import Category from "../../../models/Category";
+import dbConnect from "../../../util/dbConnect";
+
+const handler = async (req, res) => {
+  await dbConnect();
+  const { method } = req;
+
+  if (method === "GET") {
+    try {
+      const category = await Category.find();
+      res.status(200).json(category);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (method === "POST") {
+    try {
+      const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+export default handler;
