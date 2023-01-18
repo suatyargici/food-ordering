3d57ff4047acd4e7bb4b996639866398d9bc9a18
Suat-17 <yargicisuat@gmail.com>
2023-01-18 11:18:44 +0300

    chore:   add category process done

--- components/admin/Category.jsx
+++ components/admin/Category.jsx
@@ -1,35 +1,55 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Input from "../form/Input";
 import Title from "../ui/Title";
 
 const Category = () => {
   const [inputText, setInputText] = useState("");
-  const [categories, setCategories] = useState(["pizza"]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/categories`
+      );
+      setCategories(res?.data);
+    };
+    getCategories();
+  }, []);
+
+  const handleCreate = async () => {
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/categories`,
+        {
+          title: inputText,
+        }
+      );
+      setCategories([...categories, res.data]);
+      setInputText("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <div className="lg:p-8 flex-1 lg:mt-0 mt-5">
+    <div className="mt-5 flex-1 lg:mt-0 lg:p-8">
       <Title addClass="text-[40px]">Category</Title>
       <div className="mt-5">
-        <div className="flex gap-4 flex-1 items-center">
+        <div className="flex flex-1 items-center gap-4">
           <Input
             placeholder="Add a new Category..."
             onChange={(e) => setInputText(e.target.value)}
             value={inputText}
           />
-          <button
-            className="btn-primary"
-            onClick={() => {
-              setCategories([...categories, inputText]);
-              setInputText("");
-            }}
-          >
+          <button className="btn-primary" onClick={handleCreate}>
             Add
           </button>
         </div>
         <div className="mt-10">
-          {categories.map((category, index) => (
-            <div className="flex justify-between mt-4" key={index}>
-              <b className="text-xl">{category}</b>
+          {categories.map((category) => (
+            <div className="mt-4 flex justify-between" key={category._id}>
+              <b className="text-xl">{category.title}</b>
               <button
                 className="btn-primary !bg-danger"
                 onClick={() =>
