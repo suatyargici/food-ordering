6a6f6896152a727b6581340b06f6935127a950d1
Suat-17 <yargicisuat@gmail.com>
2023-01-24 15:38:32 +0300

    chore: update in order page

--- components/admin/Order.jsx
+++ components/admin/Order.jsx
@@ -1,107 +1,114 @@
+import React, { useEffect, useState } from "react";
 import Title from "../ui/Title";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const Order = () => {
-  const [orders, setOrders] = useState([]);
-  const status = ["preparing", "on the way", "delivered"];
-  useEffect(() => {
-    const getOrders = async () => {
-      try {
-        const res = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}/orders`
-        );
-        setOrders(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getOrders();
-  }, []);
+    const { push } = useRouter()
+    const [orders, setOrders] = useState([]);
+    const status = ["preparing", "on the way", "delivered"];
+    useEffect(() => {
+        const getOrders = async () => {
+            try {
+                const res = await axios.get(
+                    `${process.env.NEXT_PUBLIC_API_URL}/orders`
+                );
+                setOrders(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getOrders();
+    }, []);
 
-  const handleStatus = async (id) => {
-    const item = orders.find((order) => order._id === id);
-    const currentStatus = item.status;
+    const handleStatus = async (id) => {
+        const item = orders.find((order) => order._id === id);
+        const currentStatus = item.status;
 
-    try {
-      const res = await axios.put(
-        `${process.env.NEXT_PUBLIC_API_URL}/orders/${id}`,
-        {
-          status: currentStatus + 1,
+        try {
+            const res = await axios.put(
+                `${process.env.NEXT_PUBLIC_API_URL}/orders/${id}`,
+                {
+                    status: currentStatus + 1,
+                }
+            );
+            setOrders([res.data, ...orders.filter((order) => order._id !== id)]);
+            // setTimeout(() => {
+            //     push(`/order/${id}`)
+            // }, 1200)
+        } catch (err) {
+            console.log(err);
         }
-      );
-      setOrders([res.data, ...orders.filter((order) => order._id !== id)]);
-    } catch (err) {
-      console.log(err);
-    }
-    return (
-      <div className="mt-5 flex-1 lg:mt-0 lg:p-8">
-        <Title addClass="text-[40px]">Products</Title>
-        <div className="mt-5 w-full overflow-x-auto">
-          <table className="w-full min-w-[1000px] text-center text-sm text-gray-500">
-            <thead className="bg-gray-700 text-xs uppercase text-gray-400">
-              <tr>
-                <th scope="col" className="py-3 px-6">
-                  PRODUCT ID
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  CUSTOMER
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  TOTAL
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  PAYMENT
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  STATUS
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  ACTION
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.length > 0 &&
-                orders
-                  .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-                  .map((order) => (
-                    <tr
-                      className="border-gray-700 bg-secondary transition-all hover:bg-primary"
-                      key={order?._id}
-                    >
-                      <td className="gap-x-1 whitespace-nowrap py-4 px-6 font-medium hover:text-white ">
-                        {order?._id.substring(0, 6)}...
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 font-medium hover:text-white">
-                        {order?.customer}
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 font-medium hover:text-white">
-                        $ {order?.total}
-                      </td>
+    };
 
-                      <td className="whitespace-nowrap py-4 px-6 font-medium hover:text-white">
-                        {order?.method === 0 ? "Cash" : "Card"}
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 font-medium hover:text-white">
-                        {status[order?.status]}
-                      </td>
-                      <td className="whitespace-nowrap py-4 px-6 font-medium hover:text-white">
-                        <button
-                          className="btn-primary !bg-success"
-                          onClick={() => handleStatus(order?._id)}
-                          disabled={order?.status > 1}
-                        >
-                          Next Stage
-                        </button>
-                      </td>
+    return (
+        <div className="overflow-x-auto custom-vertical-scrollbar ml-5 mt-5" >
+            <Title className="text-center mb-5">Orders</Title>
+            <div className="overflow-x-auto custom-vertical-scrollbar max-h-[330px] mx-auto w-full ">
+                <table className="m-5 text-sm text-center text-gray-500 mx-auto rounded-t-2xl w-[670px] md:w-[940px]">
+                    <thead className="sticky top-0 z-10 text-xs text-primary bg-secondary uppercase border-white border-b-8 ">
+                        <tr>
+
+                            <th scope="col" className="py-3 px-6 hover:text-white">
+                                PRODUCT ID
+                            </th>
+                            <th scope="col" className="py-3 px-6 hover:text-white">
+                                CUSTOMER
+                            </th>
+                            <th scope="col" className="py-3 px-6 hover:text-white">
+                                TOTAL
+                            </th>
+                            <th scope="col" className="py-3 px-6 hover:text-white">
+                                PAYMENT
+                            </th>
+                            <th scope="col" className="py-3 px-6 hover:text-white">
+                                STATUS
+                            </th>
+                            <th scope="col" className="py-3 px-6 hover:text-white">
+                                ACTİON
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {orders.length > 0 &&
+                            orders
+                                .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                                .map((order) => (
+                                    <tr
+                                        key={order?._id}
+                                        className="bg-primary hover:bg-secondary transition-all border-b-white border-b-2"
+                                    >
+                                        <td className="py-4 px-6 font-medium whitespace-nowrap  hover:text-white ">
+                                            {order?._id.substring(0, 6)}...
+                                        </td>
+                                        <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                                            {order?.customer}
+                                        </td>
+                                        <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                                            ${order?.total}
+                                        </td>
+                                        <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                                            ${order?.method === 0 ? "Cash" : "Card"}
+                                        </td>
+                                        <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                                            {status[order?.status]}
+                                        </td>
+                                        <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                                            <button
+                                                className="btn-primary !bg-success"
+                                                onClick={() => handleStatus(order?._id)}
+                                                disabled={order?.status > 1}
+                                            >
+                                                Next Stage
+                                            </button>
+                        </td>
                     </tr>
-                  ))}
-            </tbody>
-          </table>
+                                ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
     );
-  };
 };
+
 export default Order;
--- models/Order.js
+++ models/Order.js
@@ -22,7 +22,6 @@ const OrderSchema = new mongoose.Schema(
     },
     method: {
       type: Number,
-      required: true,
     },
   },
   { timestamps: true }
