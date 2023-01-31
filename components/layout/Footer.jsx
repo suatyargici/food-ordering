c516bc7399fc428f0c7f552b3285ef094b1d9806
Suat-17 <yargicisuat@gmail.com>
2023-01-31 12:05:01 +0300

    feat: i created footer api

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -1,80 +1,78 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Title from "../ui/Title";
 
 const Footer = () => {
+  const [footer, setFooter] = useState([]);
+  useEffect(() => {
+    const getFooter = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/footers`
+        );
+        setFooter(res.data[0]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFooter();
+  }, []);
+
   return (
     <div className="bg-secondary text-white">
       <div className="container mx-auto pt-16 pb-6">
-        <div className="flex md:justify-between justify-center text-center flex-wrap md:gap-y-0 gap-y-6 ">
+        <div className="flex flex-wrap justify-center gap-y-6 text-center md:justify-between md:gap-y-0 ">
           <div className="md:flex-1">
             <Title addClass="text-[30px] font-dancing">Contact Us</Title>
-            <div className="flex flex-col gap-y-2 mt-3">
-              <div>
+            <div className="mt-3 flex flex-col gap-y-2">
+              <a href={footer?.location} target="_blank" rel="noreferrer">
                 <i className="fa fa-map-marker"></i>
-                <span className="inline-block ml-2">Location</span>
-              </div>
-              <div>
+                <span className="ml-2 inline-block">Location</span>
+              </a>
+              <a href={`tel:${footer?.phoneNumber}`} rel="noreferrer">
                 <i className="fa fa-phone"></i>
-                <span className="inline-block ml-2">Call +01 1234567890</span>
-              </div>
-              <div>
+                <span className="ml-2 inline-block">Call +01 1234567890</span>
+              </a>
+              <a href={`mailto:${footer?.email}`} rel="noreferrer">
                 <i className="fa fa-envelope"></i>
-                <span className="inline-block ml-2">demo@gmail.com</span>
-              </div>
+                <span className="ml-2 inline-block">demo@gmail.com</span>
+              </a>
             </div>
           </div>
           <div className="md:flex-1">
             <Title addClass="text-[38px] font-dancing">Feane</Title>
-            <p className="mt-3">
-              Necessary, making this the first true generator on the Internet.
-              It uses a dictionary of over 200 Latin words, combined with
-            </p>
-            <div className="flex items-center justify-center mt-5 gap-x-2">
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full "
-              >
-                <i className="fa fa-facebook"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full"
-              >
-                <i className="fa fa-twitter"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full"
-              >
-                <i className="fa fa-linkedin"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full"
-              >
-                <i className="fa fa-instagram"></i>
-              </a>
-              <a
-                href=""
-                className="w-8 h-8 grid place-content-center bg-white text-secondary rounded-full"
-              >
-                <i className="fa fa-pinterest"></i>
-              </a>
+            <p className="mt-3">{footer?.desc}</p>
+            <div className="mt-5 flex items-center justify-center gap-x-2">
+              {footer?.socialMedia?.map((item) => (
+                <a
+                  href={item?.link}
+                  className="grid h-8 w-8 place-content-center rounded-full bg-white text-secondary"
+                  key={item._id}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className={item.icon}></i>
+                </a>
+              ))}
             </div>
           </div>
           <div className="md:flex-1">
             <Title addClass="text-[30px] font-dancing">Opening Hours</Title>
-            <div className="flex flex-col gap-y-2 mt-3">
+            <div className="mt-3 flex flex-col gap-y-2">
               <div>
-                <span className="inline-block ml-2">Everyday</span>
+                <span className="ml-2 inline-block">
+                  {footer?.openingHours?.day}
+                </span>
               </div>
               <div>
-                <span className="inline-block ml-2">10.00 Am -10.00 Pm</span>
+                <span className="ml-2 inline-block">
+                  {footer?.openingHours?.hour}
+                </span>
               </div>
             </div>
           </div>
         </div>
-        <p className="text-center mt-10">
+        <p className="mt-10 text-center">
           © 2022 All Rights Reserved By Free Html Templates
         </p>
       </div>
