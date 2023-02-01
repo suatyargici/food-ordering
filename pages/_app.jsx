99ca8a1c40b7b06e3dfaa393f03c983bb6524847
Suat-17 <yargicisuat@gmail.com>
2023-02-01 12:11:29 +0300

    feat: added nprogess

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,12 +2,21 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/globals.css";
 import Layout from "../layout/Layout";
-
-import { Provider } from "react-redux";
+import Router from "next/router";
+import NProgress from "nprogress";
 import store from "../redux/store";
 
 import { SessionProvider } from "next-auth/react";
 import Toastify from "../components/Toastify";
+import { Provider } from "react-redux";
+
+import "../styles/globals.css";
+import "nprogress/nprogress.css";
+import "react-toastify/dist/ReactToastify.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
